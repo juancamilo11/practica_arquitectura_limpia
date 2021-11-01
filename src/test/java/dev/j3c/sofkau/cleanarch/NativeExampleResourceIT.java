760f5da53d27a580548595199a37b9e5bf46b645
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-01 18:31:24 -0500

    CreaciÃon del proyecto

---
+++ src/test/java/dev/j3c/sofkau/cleanarch/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package dev.j3c.sofkau.cleanarch;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
